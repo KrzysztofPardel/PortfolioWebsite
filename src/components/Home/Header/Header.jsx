1db03bf2c716fcafb5fa70f7c36591caad10fabc
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-06-06 10:21:15 +0200

    Navigation

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,9 +1,8 @@
 'use client';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import './Header.scss';
-
 
 import mySignBlue from '../../../../public/assets/mySignBlue.png';
 import containerWrap from './containerWrap';
@@ -13,54 +12,20 @@ export const Header = () => {
 
 	const handleToggle = () => {
 		setToggleWrap(!toggleWrap);
-		setTimeout({
-
-		},200)
+		setTimeout({}, 200);
 	};
 	return (
 		<>
 			<div id='header' className='container'>
-				<div className={toggleWrap ? 'intro' : 'intro move-up'}>
+				<div className='intro'>
 					<h1 className='title'>
 						Hi! I&apos;m <span className='blue'>Krzysztof Pardel</span>.
 					</h1>
 					<h2 className='profession'>Fontend Web Developer</h2>
-					<button onClick={handleToggle} className='invitation'>
+					<Link href='/#navigation' className='invitation'>
 						Please, come in.
-					</button>
+					</Link>
 				</div>
-				{/* <div className='container-wrap'>
-					<div className='container-left'>
-						<div className='container-signature'>
-							<Image src={mySignBlue} alt='Kris signature' className='mySignature' />
-						</div>
-					</div>
-					<div className='container-right'>
-						<div className='menu'>
-							<Link href='/#projects' className='menu-parts'>
-								<div className='text'>projects</div>
-								<div className='dot' />
-							</Link>
-							<Link href='/#skills' className='menu-parts'>
-								<div className='text'>skills</div>
-								<div className='dot' />
-							</Link>
-							<Link href='/#values' className='menu-parts'>
-								<div className='text'>values</div>
-								<div className='dot' />
-							</Link>
-							<Link href='/#about' className='menu-parts'>
-								<div className='text'>about</div>
-								<div className='dot' />
-							</Link>
-							<Link href='/#contact' className='menu-parts'>
-								<div className='text'>contact</div>
-								<div className='dot' />
-							</Link>
-						</div>
-						<div className='line' />
-					</div>
-				</div> */}
 			</div>
 		</>
 	);
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,16 +2,17 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { CgDarkMode } from 'react-icons/cg';
 import mySignBlue from '../../public/assets/mySignBlue.png';
 import './Navigation.scss';
 export const Navigation = () => {
 	return (
 		<>
-			<div className='container-nav'>
+			<div id='navigation' className='container-nav'>
 				<div className='logo'>
 					<Image src={mySignBlue} alt='Kris signature' className='mySignature' />
 				</div>
+
 				<div className='links'>
 					<Link href='/#projects' className='link'>
 						projects
@@ -28,6 +29,16 @@ export const Navigation = () => {
 					<Link href='/#contact' className='link'>
 						contact
 					</Link>
+				</div>
+				<div className='adjustments'>
+					<div className='mode'>
+						<CgDarkMode className='mode-icon' />
+					</div>
+					<div className='languages'>
+						<button className='language eng'>ENG</button>
+						<div className='line' />
+						<button className='language pol'>PL</button>
+					</div>
 				</div>
 			</div>
 		</>
