945206eb16a639c67bfdc4ba8c466ba2a4c786fa
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-06-13 22:56:15 +0200

    Projects- MUI element added

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -4,15 +4,58 @@ import './Projects.scss';
 import { motion } from 'framer-motion';
 import images from './images';
 import Image from 'next/image';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+// Tabs by Mui
+const TabPanel = (props) => {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role='tabpanel'
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+};
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired,
+};
+
+const a11yProps = (index) => {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+};
 
 export const Projects = () => {
 	const [width, setWidth] = useState(0);
 	const carousel = useRef();
+	const [value, setValue] = useState(0);
 
+	// Drag Carousel Constrains
 	useEffect(() => {
 		console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
 		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
 	}, []);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 
 	console.log(images);
 	return (
@@ -21,6 +64,26 @@ export const Projects = () => {
 				<div className='section-rectangle'>
 					<p className='section-title'>projects</p>
 				</div>
+				{/* Tabs */}
+				<Box sx={{ width: '100%' }}>
+					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+						<Tabs value={value} onChange={handleChange} aria-label='basic tabs example'>
+							<Tab label='Item One' {...a11yProps(0)} />
+							<Tab label='Item Two' {...a11yProps(1)} />
+							<Tab label='Item Three' {...a11yProps(2)} />
+						</Tabs>
+					</Box>
+					<TabPanel value={value} index={0}>
+						Item One
+					</TabPanel>
+					<TabPanel value={value} index={1}>
+						Item Two
+					</TabPanel>
+					<TabPanel value={value} index={2}>
+						Item Three
+					</TabPanel>
+				</Box>
+				{/* Carousel */}
 				<motion.div ref={carousel} className='carousel' whileTap={{ cursor: 'grabbing' }}>
 					<motion.div drag='x' dragConstraints={{ right: 0, left: -width }} className='inner-carousel'>
 						{images.map((image, i) => {
