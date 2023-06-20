68e774839bef8c5943d5a7b75aa8cd2dc23a1c46
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-06-20 16:12:16 +0200

    Contact-helper text and Email.js added

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -1,28 +1,76 @@
 'use client';
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState, useRef, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 // import Lottie from 'lottie-react';
 // import sendMessage from '../../../../public/assets/sendMessage.json';
 import './Contact.scss';
 import { userSchema } from './Validations/NameValidation';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const Contact = () => {
-	const { register, handleSubmit, errors } = useForm({
+	const form = useRef();
+	const [showConfirmation, setShowConfirmation] = useState(false);
+
+	//handling confirmation information
+	useEffect(() => {
+		AOS.init({ duration: 2000 });
+		const showConfirmation = () => {
+			
+		};
+	}, []);
+	// Handling helper texts and reset of the inputs
+	const {
+		handleSubmit,
+		control,
+		reset,
+		formState: { errors },
+	} = useForm({
 		resolver: yupResolver(userSchema),
 	});
-	const sendMessage = (data) => {
-		console.log(data);
+
+	// sending email through Email.js
+	const onSubmit = (data) => {
+		emailjs
+			.sendForm('service_be58b1t', 'template_vqos45m', form.current, '7ZBJB5X4YZcVd9q9X')
+			.then((result) => {
+				console.log(result.text);
+				reset();
+			})
+			.catch((error) => {
+				console.log(error.text);
+			});
 	};
-	// const sendMessage = async (event) => {
-	// 	event.preventDefault();
-	// 	let formData = {
-	// 		name: event.target[0].value,
-	// 		email: event.target[1].value,
-	// 		message: event.target[2].value,
-	// 	};
-	// 	const isValid = await userSchema.isValid(formData);
-	// 	console.log(isValid);
+
+	// email function
+	// const sendEmail = (e) => {
+	// 	if (e) {
+	// 		e.preventDefault();
+	// 	}
+	// 	emailjs.sendForm('service_be58b1t', 'template_vqos45m', form.current, '7ZBJB5X4YZcVd9q9X').then(
+	// 		(result) => {
+	// 			console.log(result.text);
+
+	// 		},
+	// 		(error) => {
+	// 			console.log(error.text);
+	// 		}
+	// 	);
+	// };
+	// // calling errors in helper texts
+	// const {
+	// 	handleSubmit,
+	// 	control,
+	// 	formState: { errors, dirtyFields },
+	// } = useForm({
+	// 	resolver: yupResolver(userSchema),
+	// });
+	// const onSubmit = async (data, e) => {
+	// 	e.preventDefault();
+	// 	console.log(data);
+	// 	await sendEmail();
 	// };
 
 	return (
@@ -34,14 +82,45 @@ export const Contact = () => {
 				<div className='container-contact_form'>
 					<div className='contact-animation'>{/* <Lottie animationData={sendMessage} /> */}</div>
 					<div className='line' />
-					<form onSubmit={handleSubmit(sendMessage)} className='form' ref={register}>
-						<input type='text' name='firstName' className='input ' placeholder='Name' ref={register} />
-						<p className=''>{errors.firstName?.message}</p>
-						<input type='email' name='email' className='input' placeholder='Email' ref={register} />
-						<p className=''>{errors.email?.message}</p>
-						<textarea type='text' name='message' className='textarea' placeholder='Message' ref={register} />
-						<p className=''>{errors.message?.message}</p>
+					<form ref={form} onSubmit={handleSubmit(onSubmit)} className='form'>
+						<Controller
+							name='name'
+							control={control}
+							defaultValue=''
+							render={({ field }) => (
+								<>
+									<input {...field} type='text' className='input' placeholder='Name' />
+									{errors.name && <p className='form-error'>{errors.name.message}</p>}
+								</>
+							)}
+						/>
+						<Controller
+							name='email'
+							control={control}
+							defaultValue=''
+							render={({ field }) => (
+								<>
+									<input {...field} type='email' className='input' placeholder='Email' />
+									{errors.email && <p className='form-error'>{errors.email.message}</p>}
+								</>
+							)}
+						/>
+						<Controller
+							name='message'
+							control={control}
+							defaultValue=''
+							render={({ field }) => (
+								<>
+									<textarea {...field} className='textarea' placeholder='Message' />
+									{errors.message && <p className='form-error'>{errors.message.message}</p>}
+								</>
+							)}
+						/>
 						<input type='submit' className='submit' />
+						<p className='form-message_sent'>
+							I received your message. I will reach out to you very soon!{' '}
+							<span className='form-message_sent_span'>Thank you.</span>
+						</p>
 					</form>
 				</div>
 			</div>
--- src/components/Home/Contact/Validations/NameValidation.jsx
+++ src/components/Home/Contact/Validations/NameValidation.jsx
@@ -1,8 +1,7 @@
 import * as yup from 'yup';
 
-
 export const userSchema = yup.object().shape({
-	name: yup.string().required("Please enter your name"),
-	email: yup.string().email().required("Please enter your email"),
-	message: yup.string().min(5).required("Please enter your message"),
+	name: yup.string().min(2, 'First name must be at least 3 characters long').required('Please enter your name'),
+	email: yup.string().email('Please enter a valid email').required('Please enter your email'),
+	message: yup.string().min(5, 'Message must be at least 5 characters long').required('Please enter your message'),
 });
